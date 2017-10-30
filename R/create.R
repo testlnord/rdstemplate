f803e4745bf3c811fee6722dc27480a02f283fa8
Arkady <kalakutsky@gmail.com>
2017-10-30 19:36:01 +0300

    Minor fixes

--- R/create.R
+++ R/create.R
@@ -11,16 +11,24 @@ create_rdstemplate <- function(path, use.packrat, ...){
   template.dir <- system.file('template', package = 'rdstemplate')
   template.files <- list.files(template.dir, all.files = TRUE, recursive = TRUE, include.dirs = TRUE)
   for (f in template.files){
-    finfo <- file.info(f)
-    if (finfo$is.dir){
-      dir.create(file.path(path, f))
+    template.path.f <- file.path(template.dir, f)
+    project.path.f <- file.path(path, f)
+    finfo <- file.info(template.path.f)
+    print(template.path.f)
+    if (finfo$isdir){
+      dir.create(project.path.f)
     } else {
       # Render all files as they can be templates
-      writeLines(
-        whisker::whisker.render(
-          readLines(file.path(template.dir, f)),
-          template.data),
-        file.path(path, f))
+      if (finfo$size > 0) {
+        writeLines(
+          whisker::whisker.render(
+            readLines(template.path.f),
+            template.data),
+          project.path.f)
+      } else {
+        # empty files just copy
+        file.copy(template.path.f, project.path.f, overwrite = TRUE)
+      }
     }
   }
 
