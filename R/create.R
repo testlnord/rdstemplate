86beef05dd7f10119f1544dbaebad80b1af8f1ab
Arkady <kalakutsky@gmail.com>
2017-12-01 20:06:56 +0300

    Fixes and features
    Fix lots of bugs and also add feature with default packages.

--- R/create.R
+++ R/create.R
@@ -1,41 +1,72 @@
-create_rdstemplate <- function(path, use.packrat, ...){
+create_rdstemplate <- function(path, use_packrat, install_default_packages, project_name, ...){
   # Create directory for new project
   dir.create(path, recursive=TRUE, showWarnings=TRUE)
+  print(path)
 
   # Collect all necessary data for file render
-  template.data <- as.list(match.call())
-  template.data$r.version = paste(R.version$major, R.version$minor, sep='.')
+  project.metadata <- as.list(match.call())
+  project.metadata$r_version = paste(R.version$major, R.version$minor, sep='.')
 
+
+  # initialise list of files to ignore during copying
+  if (system.file('ignore.txt', package = 'rdstemplate') != ""){
+    ignore.file.list <- readLines(system.file('ignore.txt', package = 'rdstemplate'))
+  } else {
+    ignore.file.list <- c()
+  }
 
   # Copy files from ./inst/template to new project
   template.dir <- system.file('template', package = 'rdstemplate')
   template.files <- list.files(template.dir, all.files = TRUE, recursive = TRUE, include.dirs = TRUE)
   for (f in template.files){
-    template.path.f <- file.path(template.dir, f)
-    project.path.f <- file.path(path, f)
-    finfo <- file.info(template.path.f)
-    print(template.path.f)
-    if (finfo$isdir){
-      dir.create(project.path.f)
-    } else {
-      # Render all files as they can be templates
-      if (finfo$size > 0) {
-        writeLines(
-          whisker::whisker.render(
-            readLines(template.path.f),
-            template.data),
-          project.path.f)
+    # ignore files
+    if (any(sapply(ignore.file.list, function(ignore.mask){grepl(ignore.mask, f)}))){
+      next
+    }
+    template_file_path <- file.path(template.dir, f)
+    project_file_path <- file.path(path, f)
+    finfo <- file.info(template_file_path)
+
+    if (nrow(finfo) > 0 & !is.na(finfo$isdir)){
+      if (finfo$isdir){
+        dir.create(project_file_path)
       } else {
-        # empty files just copy
-        file.copy(template.path.f, project.path.f, overwrite = TRUE)
+        # Render all files as they can be templates
+        if (finfo$size > 5){
+          writeLines(
+            whisker::whisker.render(
+              readLines(template_file_path),
+              project.metadata),
+            project_file_path)
+        } else {
+          # just copying too small files
+          file.copy(template_file_path, project_file_path)
+        }
       }
     }
   }
 
+  save(project.metadata, file=file.path(path, 'project.meta.rda'))
+
+
   # Initialize Packrat
   if (use.packrat){
-    packrat::init(path)
+    packrat::init(path, restart = FALSE)
   }
+
+  # install default packages
+  if (use.packrat & install.default.packages){
+    def.packages.file <- system.file('default_packages.txt', package = 'rdstemplate')
+    for (package.name in readLines(def.packages.file)){
+      print(file.path(path, package.name))
+      if (file.exists(package.name) & file.info(package.name)$isdir){
+        devtools::install(package.name)
+      } else {
+        install.packages(package.name)
+      }
+    }
+  }
+
 
 }
 
